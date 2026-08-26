b6dd31a1f2488f823a1de61fe95b54096d560d51
Augustin <musteryoan@gmail.com>
2026-08-26 17:36:32 +0000

    Initial FlowterServer

---
+++ src/main/kotlin/com/example/flowterserver/FlowterserverApplication.kt
@@ -0,0 +1,11 @@
+package com.example.flowterserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlowterserverApplication
+
+fun main(args: Array<String>) {
+	runApplication<FlowterserverApplication>(*args)
+}
